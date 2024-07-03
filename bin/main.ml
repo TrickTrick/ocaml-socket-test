82b3386e623475a89f9f69212c3e6a0587b9ab2f
Alex Markov <alexeymarkov.x7@gmail.com>
2024-07-04 00:41:40 +0300

    add server

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,21 @@
-let () = Printf.printf "%s\n" Socket.Test.test
+let usage_msg = "main [ARG] -c <count for child procesess>"
+let input_files = ref []
+let port = 54321
+let count = ref 1
+let anon_fun filename =
+  input_files := filename :: !input_files
+
+let speclist =
+  [("-c", Arg.Set_int count, "Set count for child procesess")]
+
+
+let () =
+  Arg.parse speclist anon_fun usage_msg;
+  (* Printf.printf "Child processes count: %d\n\r" !count;; *)
+
+  let () = Logs.set_reporter (Logs.format_reporter ()) in
+  let () = Logs.set_level (Some Logs.Info) in
+  let sock = Server.create_socket !count port in
+  let serve = Server.create_server sock in
+  Lwt_main.run @@ serve ()
+
--- lib/test.ml
+++
@@ -1,1 +0,0 @@
-let test = "Here is test module"
